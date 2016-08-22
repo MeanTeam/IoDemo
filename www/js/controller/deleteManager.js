ef4d5d00043c6d246a24e79d08e546b4e117b843
johnligithub <jxsrc.inc@gmail.com>
2016-08-22 14:01:04 -0400

    modified delete manager search

--- www/js/controller/deleteManager.js
+++ www/js/controller/deleteManager.js
@@ -12,7 +12,7 @@ angular.module('app.deleteManager', ['ionic-modal-select'])
       $scope.message = "";
       $scope.fname = "";
       $scope.lname = "";
-      $scope.search = {"fname":"","lname":"","name":""};
+      $scope.search = {"name":""};
       $scope.showSearch = false;
       $scope.showSearchButton = true;
       $scope.showMessage = !$scope.showSearch;
@@ -37,7 +37,12 @@ angular.module('app.deleteManager', ['ionic-modal-select'])
       }
       $scope.showAllManagers = function() {
         $scope.showAll = true;
-        $scope.managers = $scope.allManagers;
+        $scope.managers = [];
+        Object.keys($scope.allManagers).forEach(function (key) {
+          var mgr = $scope.allManagers[key];
+          mgr = markFound(mgr, "");
+          $scope.managers.push(mgr);
+        });
         displaySearchResult();
 
       }
@@ -48,30 +53,28 @@ angular.module('app.deleteManager', ['ionic-modal-select'])
         $scope.managers = [];
         Object.keys($scope.allManagers).forEach(function (key) {
           var mgr = $scope.allManagers[key];
-          var name = mgr.lname+", "+mgr.fname;
+          var name = mgr.fname+" "+mgr.lname;
           var nmatch = $scope.search.name.length ==0 ||
             name.toLowerCase().indexOf($scope.search.name.toLowerCase()) >= 0;
-//            mgr.lname.toLowerCase().indexOf($scope.search.name.toLowerCase()) >= 0 ||
-//            mgr.fname.toLowerCase().indexOf($scope.search.name.toLowerCase()) >= 0;
-/*
-          var lmatch = $scope.search.lname.length ==0 ||
-            mgr.lname.toLowerCase().indexOf($scope.search.lname.toLowerCase()) == 0;
-          var fmatch = $scope.search.fname.length ==0 ||
-            mgr.fname.toLowerCase().indexOf($scope.search.fname.toLowerCase()) == 0;
-*/          var empty = isEmpty();
-//          if(!empty && fmatch && lmatch) {
+          var empty = isEmpty();
           if(!isEmpty() && nmatch) {
-            $scope.managers.push($scope.allManagers[key]);
+            var mgr = $scope.allManagers[key];
+            mgr = markFound(mgr, $scope.search.name);
+            $scope.managers.push(mgr);
           }
         });
         if(isEmpty() && $scope.showAll) {
-          $scope.managers = $scope.allManagers;
+          Object.keys($scope.allManagers).forEach(function (key) {
+            var mgr = $scope.allManagers[key];
+            mgr = markFound(mgr, "");
+            $scope.managers.push(mgr);
+          });
         }
         displaySearchResult();
       };
 
       function isEmpty() {
-        return $scope.search.name.length ==0; // ($scope.search.lname.length ==0 && $scope.search.fname.length ==0);
+        return $scope.search.name.length ==0;
       }
       function displaySearchResult() {
         if($scope.managers.length > 0) {
@@ -89,7 +92,6 @@ angular.module('app.deleteManager', ['ionic-modal-select'])
       $scope.delete = function() {
         var msg = "";
         var deleteManager = [];
-console.log($scope.managers);
         Object.keys($scope.managers).forEach(function (key) {
           var mgr = $scope.managers[key];
           if(mgr.delete){
@@ -106,7 +108,6 @@ console.log($scope.managers);
             title: '<b>Delete Manager</b>',
             template: msg
           });
-          console.log(confirmPopup);
           confirmPopup.then(function (res) {
             if(res) {
               $scope.search.fname = "";
@@ -125,7 +126,6 @@ console.log($scope.managers);
                   }
                 });
                 $scope.managers = newManagers;
-                console.log("newManagers: "+newManagers);
                 $scope.allManagers = newManagers;
                 displaySearchResult();
               }, function(error) {
@@ -137,6 +137,20 @@ console.log($scope.managers);
         }
       };
 
+      function markFound(mgr, found) {
+        var fullname = mgr.fname+' '+mgr.lname;
+        mgr["name"] = {"first":mgr.fname,"found":" ","second":mgr.lname};
+        if(found != undefined && found.trim().length > 0) {
+          var i = fullname.toLowerCase().indexOf(found);
+          if(i > -1) {
+            var f = fullname.substring(0,i);
+            var m = fullname.substring(i, i+found.length);
+            var l = fullname.substring(i+found.length, fullname.length);
+            mgr["name"] = {"first":f,"found":m,"second":l};
+          }
+        }
+        return mgr;
+      }
       $scope.$on('$ionicView.beforeEnter', function () {
         SISOSprints.getManagerList({}, function (mgrs) {
           Object.keys(mgrs).forEach(function(key) {
@@ -145,6 +159,7 @@ console.log($scope.managers);
               SISOSprints.getUsersByManager({mfname: mgr.fname, mlname: mgr.lname}, function (success) {
                 if(success.length === 0) {
                   mgr.delete = false;
+                  mgr = markFound(mgr,"");
                   $scope.managers.push(mgr);
                   $scope.allManagers.push(mgr);
                 }
