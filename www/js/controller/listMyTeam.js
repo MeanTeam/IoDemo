db54ccf08dde96eb5f53877463193c357bc49693
MeanTeam <mean.team.lm@gmail.com>
2017-05-17 11:44:19 -0400

    Added listMyTeam.js
    Added a controller to list the reporters in My Team tab for the Manager.

---
+++ www/js/controller/listMyTeam.js
@@ -0,0 +1,32 @@
+angular.module('app.listMyTeam', ['ionic-modal-select'])
+
+
+  .controller('listMyTeamCtrl', ['$scope', 'SISOSprints', '$ionicLoading', 'ProfileFactory', '$filter',
+    function($scope, SISOSprints, $ionicLoading, ProfileFactory, $filter){
+
+      $scope.user = {fname: '', lname: ''};
+      $scope.records = [];
+      $scope.noReporters = false;
+
+      $scope.$on('$ionicView.beforeEnter', function () {
+         $scope.user.fname = ProfileFactory.getProfile().fname;
+         $scope.user.lname = ProfileFactory.getProfile().lname;
+
+        if($scope.user.fname !== '' &&  $scope.user.lname !== '') {
+
+          SISOSprints.get({mfname: ProfileFactory.getProfile().fname, mlname: ProfileFactory.getProfile().lname}, function (recs) {
+            if (typeof recs !== undefined && recs.length > 0) {
+              $scope.records = recs;
+            }else{
+              $scope.noReporters = true;
+              //$ionicLoading.show({template: 'No Users Signed In!', noBackdrop: true, duration: 2200});
+            }
+
+          });
+        }else{
+          $ionicLoading.show({template: 'User name must not be empty!', noBackdrop: true, duration: 2200});
+        }
+
+      });
+
+    }]);
