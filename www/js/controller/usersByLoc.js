2eef1424f14186829bc039e575dfb419bf81de01
Hari Kothurthi <k.haribabu@gmail.com>
2017-06-23 00:03:47 -0400

    Changes for After Hours to use correct location

--- www/js/controller/usersByLoc.js
+++ www/js/controller/usersByLoc.js
@@ -12,19 +12,44 @@ angular.module('app.usersByLoc', ['ionic-modal-select'])
       });
 
       $scope.getUsersByLoc = function () {
+          var _fname = ProfileFactory.getProfile().fname;
+          var _lname = ProfileFactory.getProfile().lname;
+          var _mname = ProfileFactory.getProfile().mname;
+          var _loc = '';
 
-        $scope.location = ProfileFactory.getProfile().preferredLocation;
+          if (_fname !== '' && _lname !== '') {
 
-        if($scope.location) {
-          SISOSprints.usersByLoc({location: encodeURI($scope.location)}, function (rec) {
-            $scope.count = rec.count;
-            $scope.records = rec.users;
+            SISOSprints.get({
+              fname: _fname,
+              lname: _lname,
+              mname: _mname
+            }, function (recs) {
+              if (typeof recs !== undefined && recs.length > 0) {
+                $scope.records = recs;
+                
+              } else {
+
+                $scope.reporters = true;
+                $scope.$broadcast('scroll.refreshComplete');
+                $ionicLoading.show({template: 'Sign-In to view information!', noBackdrop: true, duration: 2200});
+              }
+              $scope.location = recs[0].location;
+              if($scope.location) {
+                 SISOSprints.usersByLoc({location: encodeURI($scope.location)}, function (rec) {
+                 $scope.count = rec.count;
+                 $scope.records = rec.users;
+                 $scope.$broadcast('scroll.refreshComplete');
+                  });
+                }else{
+                  $scope.$broadcast('scroll.refreshComplete');
+                  $ionicLoading.show({template: 'Location must not be empty!', noBackdrop: true, duration: 2200});
+              }
+              $scope.$broadcast('scroll.refreshComplete');
+            });
+          } else {
             $scope.$broadcast('scroll.refreshComplete');
-          });
-        }else{
-          $scope.$broadcast('scroll.refreshComplete');
-          $ionicLoading.show({template: 'Location must not be empty!', noBackdrop: true, duration: 2200});
-        }
+            $ionicLoading.show({template: 'User name must not be empty!', noBackdrop: true, duration: 2200});
+          };
       }
 
     }]);
