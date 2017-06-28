33d8577538deac5846e51ed906a4c4d236ceb355
Wilfredo Villegas <wvillegasm@gmail.com>
2017-06-28 03:29:06 -0400

    Cleaning up - After hours

--- www/js/controller/usersByLoc.js
+++ www/js/controller/usersByLoc.js
@@ -1,7 +1,7 @@
-angular.module('app.usersByLoc', ['ionic-modal-select'])
+angular.module('app.usersByLoc', [ 'ionic-modal-select' ])
 
-  .controller('usersByLocCtrl', ['$scope', 'SISOSprints', '$ionicLoading', 'ProfileFactory',
-    function($scope, SISOSprints, $ionicLoading, ProfileFactory){
+  .controller('usersByLocCtrl', [ '$scope', 'SISOSprints', '$ionicLoading', 'ProfileFactory',
+    function ($scope, SISOSprints, $ionicLoading, ProfileFactory) {
 
       $scope.location = '';
       $scope.count = 0;
@@ -12,44 +12,45 @@ angular.module('app.usersByLoc', ['ionic-modal-select'])
       });
 
       $scope.getUsersByLoc = function () {
-          var _fname = ProfileFactory.getProfile().fname;
-          var _lname = ProfileFactory.getProfile().lname;
-          var _mname = ProfileFactory.getProfile().mname;
-          var _loc = '';
+        var _fname = ProfileFactory.getProfile().fname;
+        var _lname = ProfileFactory.getProfile().lname;
+        var _mname = ProfileFactory.getProfile().mname;
+        var _loc = '';
 
-          if (_fname !== '' && _lname !== '') {
+        if (_fname !== '' && _lname !== '') {
 
-            SISOSprints.get({
-              fname: _fname,
-              lname: _lname,
-              mname: _mname
-            }, function (recs) {
-              if (typeof recs !== undefined && recs.length > 0) {
-                $scope.records = recs;
-                
+          SISOSprints.get({
+            fname: _fname,
+            lname: _lname,
+            mname: _mname
+          }, function (recs) {
+
+            if (typeof recs !== undefined && recs.length > 0) {
+              $scope.location = recs[ 0 ].location
+              if ($scope.location) {
+                SISOSprints.usersByLoc({location: encodeURI($scope.location)}, function (rec) {
+                  $scope.count = rec.count;
+                  $scope.records = rec.users;
+                  //$scope.$broadcast('scroll.refreshComplete');
+                });
               } else {
-
-                $scope.reporters = true;
-                $scope.$broadcast('scroll.refreshComplete');
-                $ionicLoading.show({template: 'Sign-In to view information!', noBackdrop: true, duration: 2200});
-              }
-              $scope.location = recs[0].location;
-              if($scope.location) {
-                 SISOSprints.usersByLoc({location: encodeURI($scope.location)}, function (rec) {
-                 $scope.count = rec.count;
-                 $scope.records = rec.users;
-                 $scope.$broadcast('scroll.refreshComplete');
-                  });
-                }else{
-                  $scope.$broadcast('scroll.refreshComplete');
-                  $ionicLoading.show({template: 'Location must not be empty!', noBackdrop: true, duration: 2200});
+                //$scope.$broadcast('scroll.refreshComplete');
+                $ionicLoading.show({template: 'Location must not be empty!', noBackdrop: true, duration: 2200});
               }
               $scope.$broadcast('scroll.refreshComplete');
-            });
-          } else {
-            $scope.$broadcast('scroll.refreshComplete');
-            $ionicLoading.show({template: 'User name must not be empty!', noBackdrop: true, duration: 2200});
-          };
+
+            } else {
+              $scope.reporters = true;
+              $scope.$broadcast('scroll.refreshComplete');
+              $ionicLoading.show({template: 'Sign-In to view information!', noBackdrop: true, duration: 2200});
+            }
+
+          });
+        } else {
+          $scope.$broadcast('scroll.refreshComplete');
+          $ionicLoading.show({template: 'User name must not be empty!', noBackdrop: true, duration: 2200});
+        }
+
       }
 
-    }]);
+    } ]);
